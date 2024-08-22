da95e79a3af6e897ce13032649ec934b2bae8310
VedangKanade <vedangk20@gmail.com>
2024-08-22 15:34:52 +0530

    Demo

--- salonManagement/src/App.jsx
+++ salonManagement/src/App.jsx
@@ -4,6 +4,12 @@ function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline bg-red-500">Hello world!</h1>
+      <p className="bg-yellow-400 italic text-xl">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias
+        molestias quod quis expedita, eligendi dolorum deserunt ea corrupti,
+        blanditiis minus ducimus cupiditate eveniet autem! Nam nihil qui
+        necessitatibus voluptatum suscipit.
+      </p>
     </>
   );
 }
