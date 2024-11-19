ca14f3fa58ec3194bc3067fdc0a74b40c858aef3
VedangKanade <vedangk20@gmail.com>
2024-11-19 18:31:48 +0530

    Header started

--- salonManagement/src/App.jsx
+++ salonManagement/src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
+      <Header />
       <h1 className="text-3xl font-bold underline bg-red-500">Hello world!</h1>
       <p className="bg-yellow-400 italic text-xl">
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias
--- salonManagement/src/components/Header.jsx
+++ salonManagement/src/components/Header.jsx
@@ -1,8 +1,9 @@
-
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className="bg-yellow-400 text-blue-100 text-center text-2xl">
+      This is the header
+    </div>
+  );
+};
 
-export default Header
+export default Header;
